9d2fd92d9976666e26880e43c85aad04b7d8d388
vmisyats <vmisyats@localhost>
2011-05-22 12:54:00 +0300

    Added toString() helper method

--- Astromaximum/src/com/astromaximum/util/Event.java
+++ Astromaximum/src/com/astromaximum/util/Event.java
@@ -3,74 +3,130 @@ package com.astromaximum.util;
 import java.util.*;
 
 final public class Event {
-    static final byte SE_SUN = 0;
-    static final byte SE_MOON = 1;
-    static final byte SE_MERCURY = 2;
-    static final byte SE_VENUS = 3;
-    static final byte SE_MARS = 4;
-    static final byte SE_JUPITER = 5;
-    static final byte SE_SATURN = 6;
-    static final byte SE_URANUS = 7;
-    static final byte SE_NEPTUNE = 8;
-    static final byte SE_PLUTO = 9;
-    static final byte SE_TRUE_NODE = 10;
-    static final byte SE_MEAN_APOG = 11;
-    static final byte SE_WHITE_MOON = 12;
-    static final int EV_VOC = 0; // void of course
-    static final int EV_SIGN_ENTER = 1; // enter into sign
-    static final int EV_ASP_EXACT = 2; // exact aspect
-    static final int EV_RISE = 3;  // rising & setting
-    static final int EV_DEGREE_PASS = 4;  // entering degree
-    static final int EV_VIA_COMBUSTA = 5;  // good & bad degrees
-    static final int EV_RETROGRADE = 6;
-    static final int EV_ECLIPSE = 7;
-    static final int EV_TITHI = 8;
-    static final int EV_NAKSHATRA = 9;
-    static final int EV_SET = 10;  // rising & setting
-    static final int EV_DECL_EXACT = 11;  // declination
-    static final int EV_NAVROZ = 12;  // Navroz
-    static final int EV_TOP_DAY = 13;  // week days
-    static final int EV_PLANET_HOUR = 14;  // planetary hours
-    static final int EV_STATUS = 15;
-    static final int EV_SUN_RISE = 16;
-    static final int EV_MOON_RISE = 17;
-    static final int EV_MOON_MOVE = 18;
-    static final int EV_SEL_DEGREES = 19;
-    static final int EV_DAY_HOURS = 20;
-    static final int EV_NIGHT_HOURS = 21;
-    static final int EV_SUN_DAY = 22;
-    static final int EV_MOON_DAY = 23;
-    static final int EV_TOP_MONTH = 24;
-    static final int EV_MOON_PHASE = 25;
-    static final int EV_ZODIAC_SIGN = 26;
-    static final int EV_PANEL = 27;
-    static final int EV_TOPIC_BUTTON = 28;
-    static final int EV_DEG_2ND = 29; // degrees on second page
-    static final int EV_WEEK_GRID = 30;
-    static final int EV_MONTH_GRID = 31;
-    static final int EV_DECUMBITURE = 32;
-    static final int EV_DECUMB_ASPECT = 33;
-    static final int EV_DECUMB_BEGIN = 34;
-    static final int EV_SUN_DEGREE_LARGE = 35;
-    static final int EV_MOON_SIGN_LARGE = 36;
-    static final int EV_HELP = 37;
-    static final int EV_ASP_EXACT_MOON = 38;
-    static final int EV_DEGPASS0 = 39;
-    static final int EV_DEGPASS1 = 40;
-    static final int EV_DEGPASS2 = 41;
-    static final int EV_DEGPASS3 = 42;
-    static final int EV_HELP0 = 43;
-    static final int EV_HELP1 = 44;
-    static final int EV_ASTRORISE = 45;
-    static final int EV_ASTROSET = 46;
-    static final int EV_APHETICS = 47;
-    static final int EV_FAST = 48;
-    static final int EV_ASCAPHETICS = 49;
-    static final int EV_MSG = 50;
-    static final int EV_BACK = 51;
-    static final int EV_TATTVAS = 52;
-    static final int EV_LAST = 53;  // last - do not use
+    public static final byte SE_SUN = 0;
+    public static final byte SE_MOON = 1;
+    public static final byte SE_MERCURY = 2;
+    public static final byte SE_VENUS = 3;
+    public static final byte SE_MARS = 4;
+    public static final byte SE_JUPITER = 5;
+    public static final byte SE_SATURN = 6;
+    public static final byte SE_URANUS = 7;
+    public static final byte SE_NEPTUNE = 8;
+    public static final byte SE_PLUTO = 9;
+    public static final byte SE_TRUE_NODE = 10;
+    public static final byte SE_MEAN_APOG = 11;
+    public static final byte SE_WHITE_MOON = 12;
+    public static final int EV_VOC = 0; // void of course
+    public static final int EV_SIGN_ENTER = 1; // enter into sign
+    public static final int EV_ASP_EXACT = 2; // exact aspect
+    public static final int EV_RISE = 3;  // rising & setting
+    public static final int EV_DEGREE_PASS = 4;  // entering degree
+    public static final int EV_VIA_COMBUSTA = 5;  // good & bad degrees
+    public static final int EV_RETROGRADE = 6;
+    public static final int EV_ECLIPSE = 7;
+    public static final int EV_TITHI = 8;
+    public static final int EV_NAKSHATRA = 9;
+    public static final int EV_SET = 10;  // rising & setting
+    public static final int EV_DECL_EXACT = 11;  // declination
+    public static final int EV_NAVROZ = 12;  // Navroz
+    public static final int EV_TOP_DAY = 13;  // week days
+    public static final int EV_PLANET_HOUR = 14;  // planetary hours
+    public static final int EV_STATUS = 15;
+    public static final int EV_SUN_RISE = 16;
+    public static final int EV_MOON_RISE = 17;
+    public static final int EV_MOON_MOVE = 18;
+    public static final int EV_SEL_DEGREES = 19;
+    public static final int EV_DAY_HOURS = 20;
+    public static final int EV_NIGHT_HOURS = 21;
+    public static final int EV_SUN_DAY = 22;
+    public static final int EV_MOON_DAY = 23;
+    public static final int EV_TOP_MONTH = 24;
+    public static final int EV_MOON_PHASE = 25;
+    public static final int EV_ZODIAC_SIGN = 26;
+    public static final int EV_PANEL = 27;
+    public static final int EV_TOPIC_BUTTON = 28;
+    public static final int EV_DEG_2ND = 29; // degrees on second page
+    public static final int EV_WEEK_GRID = 30;
+    public static final int EV_MONTH_GRID = 31;
+    public static final int EV_DECUMBITURE = 32;
+    public static final int EV_DECUMB_ASPECT = 33;
+    public static final int EV_DECUMB_BEGIN = 34;
+    public static final int EV_SUN_DEGREE_LARGE = 35;
+    public static final int EV_MOON_SIGN_LARGE = 36;
+    public static final int EV_HELP = 37;
+    public static final int EV_ASP_EXACT_MOON = 38;
+    public static final int EV_DEGPASS0 = 39;
+    public static final int EV_DEGPASS1 = 40;
+    public static final int EV_DEGPASS2 = 41;
+    public static final int EV_DEGPASS3 = 42;
+    public static final int EV_HELP0 = 43;
+    public static final int EV_HELP1 = 44;
+    public static final int EV_ASTRORISE = 45;
+    public static final int EV_ASTROSET = 46;
+    public static final int EV_APHETICS = 47;
+    public static final int EV_FAST = 48;
+    public static final int EV_ASCAPHETICS = 49;
+    public static final int EV_MSG = 50;
+    public static final int EV_BACK = 51;
+    public static final int EV_TATTVAS = 52;
+    public static final int EV_LAST = 53;  // last - do not use
 
+    private static final String[] evTypeStr = {
+        "EV_VOC", // 0; // void of course
+        "EV_SIGN_ENTER", // 1; // enter into sign
+        "EV_ASP_EXACT", // 2; // exact aspect
+        "EV_RISE", // 3;  // rising & setting
+        "EV_DEGREE_PASS", // 4;  // entering degree
+        "EV_VIA_COMBUSTA", // 5;  // good & bad degrees
+        "EV_RETROGRADE", // 6;
+        "EV_ECLIPSE", // 7;
+        "EV_TITHI", // 8;
+        "EV_NAKSHATRA", // 9;
+        "EV_SET", // 10;  // rising & setting
+        "EV_DECL_EXACT", // 11;  // declination
+        "EV_NAVROZ", // 12;  // Navroz
+        "EV_TOP_DAY", // 13;  // week days
+        "EV_PLANET_HOUR", // 14;  // planetary hours
+        "EV_STATUS", // 15;
+        "EV_SUN_RISE", // 16;
+        "EV_MOON_RISE", // 17;
+        "EV_MOON_MOVE", // 18;
+        "EV_SEL_DEGREES", // 19;
+        "EV_DAY_HOURS", // 20;
+        "EV_NIGHT_HOURS", // 21;
+        "EV_SUN_DAY", // 22;
+        "EV_MOON_DAY", // 23;
+        "EV_TOP_MONTH", // 24;
+        "EV_MOON_PHASE", // 25;
+        "EV_ZODIAC_SIGN", // 26;
+        "EV_PANEL", // 27;
+        "EV_TOPIC_BUTTON", // 28;
+        "EV_DEG_2ND", // 29; // degrees on second page
+        "EV_WEEK_GRID", // 30;
+        "EV_MONTH_GRID", // 31;
+        "EV_DECUMBITURE", // 32;
+        "EV_DECUMB_ASPECT", // 33;
+        "EV_DECUMB_BEGIN", // 34;
+        "EV_SUN_DEGREE_LARGE", // 35;
+        "EV_MOON_SIGN_LARGE", // 36;
+        "EV_HELP", // 37;
+        "EV_ASP_EXACT_MOON", // 38;
+        "EV_DEGPASS0", // 39;
+        "EV_DEGPASS1", // 40;
+        "EV_DEGPASS2", // 41;
+        "EV_DEGPASS3", // 42;
+        "EV_HELP0", // 43;
+        "EV_HELP1", // 44;
+        "EV_ASTRORISE", // 45;
+        "EV_ASTROSET", // 46;
+        "EV_APHETICS", // 47;
+        "EV_FAST", // 48;
+        "EV_ASCAPHETICS", // 49;
+        "EV_MSG", // 50;
+        "EV_BACK", // 51;
+        "EV_TATTVAS", // 52;
+        "EV_LAST", // 53;  // last - do not use
+    };
     // Any changes above must be synched with %eventType in tools.pm
     // and EventType in mutter2/events.h !!!
 
@@ -93,9 +149,16 @@ final public class Event {
      * @param dat
      * @param planet
      */
-    Event(long dat, int planet) {
+    Event(long date, int planet) {
         mPlanet0 = (byte) planet;
-        mDate0 = mDate1 = dat;
+        mDate0 = mDate1 = date;
+    }
+
+    public Event(int evType, long date0, long date1, int planet0, int planet1, int degree) {
+    	mEvtype = evType;
+    	mDate0 = date0; mDate1 = date1;
+        mPlanet0 = (byte)planet0; mPlanet1 = (byte)planet1;
+        mDegree = (short)degree;
     }
 
     /**
@@ -137,6 +200,12 @@ final public class Event {
 		System.out.println("INSERT INTO common_events (evtype, date0, date1, degree, planet0, planet1) VALUES ("
 				+ mEvtype + ", julianday('" + long2String(mDate0, 0, false) + "', 'utc'), julianday('"
 				+ long2String(mDate1, 0, false) + "', 'utc') , " + mDegree + ", " + mPlanet0 + ", " + mPlanet1 + ");");
+	}
+
+	public String toString() {
+		return "Event: (" + mEvtype + " " + getEvTypeStr() + ", '" + 
+				long2String(mDate0, 0, false) + "', '" + long2String(mDate1, 0, false) + 
+				"', " + mPlanet0 + ", " + mPlanet1 + ", " + mDegree + ")";
 	}
 
 	public String date2Sql (long date)
@@ -223,6 +292,10 @@ final public class Event {
 
 	public void setFullDegree(short degree) {
 		mDegree = degree;
+	}
+	
+	public String getEvTypeStr() {
+		return evTypeStr[mEvtype];
 	}
 }
 
