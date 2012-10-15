fc15252699daf24b90178f5365def97bafe95ccd
willow <willow@localhost>
2012-10-15 20:15:03 +0300

    Removed unused files

--- Astromaximum/src/com/astromaximum/android/EventActivity.java
+++
@@ -1,7 +0,0 @@
-package com.astromaximum.android;
-
-import android.app.Activity;
-
-public class EventActivity extends Activity {
-
-}
--- Astromaximum/src/com/astromaximum/util/EventConsumer.java
+++
@@ -1,5 +0,0 @@
-package com.astromaximum.util;
-
-public interface EventConsumer {
-	void addEvent(Event event);
-}
