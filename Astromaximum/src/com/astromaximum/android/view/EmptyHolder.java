33462dd7d2f23c92a2c0fed69961055835ecf534
willow <willow@localhost>
2012-11-20 21:51:53 +0200

    Issue #22 Missing files added

---
+++ Astromaximum/src/com/astromaximum/android/view/EmptyHolder.java
@@ -0,0 +1,31 @@
+package com.astromaximum.android.view;
+
+import com.astromaximum.android.R;
+import com.astromaximum.util.Event;
+
+public class EmptyHolder extends ViewHolder {
+
+	public EmptyHolder(SummaryItem si) {
+		super(si, R.layout.item_empty, LAYOUT_FLAG_TEXT0);
+	}
+
+	@Override
+	public void fillLayout() {
+		int textId = 0;
+		switch (mSummaryItem.mKey) {
+		case Event.EV_VOC:
+			textId = R.string.no_voc;
+			break;
+		case Event.EV_VIA_COMBUSTA:
+			textId = R.string.no_vc;
+			break;
+		case Event.EV_ASP_EXACT:
+			textId = R.string.no_aspects;
+			break;
+		}
+		if (textId == 0)
+			mText0.setText("");
+		else
+			mText0.setText(textId);
+	}
+}
