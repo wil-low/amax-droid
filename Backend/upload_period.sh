1a617f3f5efb8bdb776abfe5800a97b739c1569e
willow <willow@localhost>
2013-04-12 19:51:40 +0300

    upload script issue comment

--- Backend/upload_period.sh
+++ Backend/upload_period.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+# Slow login solved by setting:
+# AddressFamily inet
+# in /etc/ssh_config
+
 ARCFILE=$1.tgz
 HOST=w_astromaximum-com_79c70237@astromaximum.com
 
-scp deploy/$ARCFILE $HOST:arc/ && ssh $HOST tar xzfv arc/$ARCFILE -C http/data/
+scp deploy/$ARCFILE $HOST:arc/  &&  ssh $HOST tar xzfv arc/$ARCFILE -C http/data/
